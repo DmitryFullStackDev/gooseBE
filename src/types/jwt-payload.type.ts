902ecf3b8736e9f1116fd427930a3ee2824adc7d
dmitryReact <dmitry.dev.react@gmail.com>
2025-05-21 00:32:48 +0300

    types

---
+++ src/types/jwt-payload.type.ts
@@ -0,0 +1,5 @@
+export type JwtPayload = {
+    userId: number;
+    username: string;
+    role: string;
+} 
---
+++ src/types/tap-response.type.ts
@@ -0,0 +1,5 @@
+export type TapResponse = {
+    message: string;
+    tapsCount: number;
+    points: number;
+} 
